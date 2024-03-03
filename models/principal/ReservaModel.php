f1ccb381a82b7c7a7dcea6e68b67f8e3500e8a97
mattmosz <matias.mosquera619@gmail.com>
2024-03-03 16:12:58 -0500

    fix: query habitaciones

--- models/principal/ReservaModel.php
+++ models/principal/ReservaModel.php
@@ -17,11 +17,11 @@ class ReservaModel extends Query{
     }
     //OBTENER HABITACIONES
     public function getHabitaciones(){
-        return $this->todos("SELECT * FROM habitaciones WHERE estado = 1");
+        return $this->todos("SELECT * FROM habitaciones WHERE estado_habitacion = 1");
     }
      //OBTENER HABITACION
      public function getHabitacion($id_habitacion){
-        return $this->todos("SELECT * FROM habitaciones WHERE id = $id_habitacion");
+        return $this->todos("SELECT * FROM habitaciones WHERE id_habitacion = $id_habitacion");
     }
 }
 
