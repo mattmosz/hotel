44f2b8fbb1785cf257ce24cc446f511fd67fae85
XavierTorres <xavier-torres99@hotmail.com>
2024-03-05 16:56:20 -0500

    fix: if acceso formulario

--- assets/principal/js/pages/registro.js
+++ assets/principal/js/pages/registro.js
@@ -1,7 +1,15 @@
 const frm = document.querySelector('#formulario');
 document.addEventListener('DOMContentLoaded', function() {
      frm.addEventListener('submit', function(e) {
-       
+       e.preventDefault(); 
+       if(frm.nombre.value == '' || frm.apellido.value == ''
+       || frm.usuario.value == '' || frm.correo.value == ''
+       || frm.clave.value == '' || frm.confirmar.value == ''){
+
+       }else{
+
+       }
+
     }) 
 });
     
