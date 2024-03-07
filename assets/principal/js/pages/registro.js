406d9cf3b013ba003d519097542e5e1cb46e3b0b
mattmosz <matias.mosquera619@gmail.com>
2024-03-07 14:22:51 -0500

    fix: alertas de registro de usuario

--- assets/principal/js/pages/registro.js
+++ assets/principal/js/pages/registro.js
@@ -3,35 +3,37 @@ const terminos = document.querySelector("#chb2");
 document.addEventListener("DOMContentLoaded", function () {
   frm.addEventListener("submit", function (e) {
     e.preventDefault();
-    // if(
-    //  frm.nombre.value == ''  ||
-    //  frm.apellido.value == ''||
-    //  frm.usuario.value == '' ||
-    //   frm.correo.value == ''  ||
-    //   frm.clave.value == ''   ||
-    //   frm.confirmar.value == ''
-    //   ) {
-    //   aleraSW('TODOS LOS CAMPOS SON OBLIGATORIOS', 'warning');
-    // }else if (!terminos.ariaChecked){
-    //   aleraSW('ACEPTAR LOS TERMINOS Y CONDICIONES', 'warning');
-    //  }else{
-    const http = new XMLHttpRequest();
-    const url = base_url + "registro/crear";
-    http.open("POST", url, true);
-    http.send(new FormData(frm));
-    http.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText);        
-        const res = JSON.parse(this.responseText);
-        sweetalert(res.msg, res.tipo);
-        if (res.tipo == "success") {
-          frm.reset();
-          //Redirigir
-
-        } 
+    if (
+      frm.nombre.value == "" ||
+      frm.apellido.value == "" ||
+      frm.usuario.value == "" ||
+      frm.correo.value == "" ||
+      frm.clave.value == "" ||
+      frm.confirmar.value == ""
+    ) {
+      sweetalert("TODOS LOS CAMPOS SON OBLIGATORIOS", "warning");
+    } else if (!terminos.checked) {
+      sweetalert("ACEPTAR LOS TERMINOS Y CONDICIONES", "warning");
+    } else {
+      if (frm.clave.value == frm.confirmar.value) {
+        const http = new XMLHttpRequest();
+        const url = base_url + "registro/crear";
+        http.open("POST", url, true);
+        http.send(new FormData(frm));
+        http.onreadystatechange = function () {
+          if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+            const res = JSON.parse(this.responseText);
+            sweetalert(res.msg, res.tipo);
+            if (res.tipo == "success") {
+              frm.reset();
+              //Redirigir
+            }
+          }
+        };
+      } else {
+        sweetalert("LAS CONTRASEÑAS NO COINCIDEN", "warning");
       }
-     };
-    //}
-
+    }
   });
 });
