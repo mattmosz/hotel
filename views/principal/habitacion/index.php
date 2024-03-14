ee092d61e46668620eafd98a94bae897ee92ad8d
mattmosz <matias.mosquera619@gmail.com>
2024-03-14 14:14:39 -0500

    fix: index de habitaciones

--- views/principal/habitacion/index.php
+++ views/principal/habitacion/index.php
@@ -1,43 +1,7 @@
 <?php include_once 'views/template/header-principal.php';
 include_once 'views/template/portada.php'; ?>
 
-<section class="our-rooms-area-two our-rooms-area-four ptb-100">
-    <div class="container">
-        <div class="tab industries-list-tab">
-            <div class="row">
-                <div class="col-lg-5">
-                    <div class="col-lg-7">
-                        <div class="tab_content">
-                            <div class="tabs_item">
-                                <div class="our-rooms-single-img room-bg-1">
-                                </div>
-                            </div>
-                            <div class="tabs_item">
-                                <div class="our-rooms-single-img room-bg-2">
-                                </div>
-                            </div>
-                            <div class="tabs_item">
-                                <div class="our-rooms-single-img room-bg-3">
-                                </div>
-                            </div>
-                            <div class="tabs_item">
-                                <div class="our-rooms-single-img room-bg-4">
-                                </div>
-                            </div>
-                            <div class="tabs_item">
-                                <div class="our-rooms-single-img room-bg-5">
-                                </div>
-                            </div>
-                            <div class="tabs_item">
-                                <div class="our-rooms-single-img room-bg-6">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-</section>
+<section class="our-rooms-area-two our-rooms-area-four ptb-100"></section>
 
 <section class="our-rooms-area pb-100">
     <div class="container">
@@ -236,33 +200,6 @@ include_once 'views/template/portada.php'; ?>
                             </a>
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="col-lg-12">
-                <div class="page-navigation-area">
-                    <nav aria-label="Page navigation example text-center">
-                        <ul class="pagination">
-                            <li class="page-item">
-                                <a class="page-link page-links" href="#">
-                                    <i class='bx bx-chevrons-left'></i>
-                                </a>
-                            </li>
-                            <li class="page-item active">
-                                <a class="page-link" href="#">1</a>
-                            </li>
-                            <li class="page-item">
-                                <a class="page-link" href="#">2</a>
-                            </li>
-                            <li class="page-item">
-                                <a class="page-link" href="#">3</a>
-                            </li>
-                            <li class="page-item">
-                                <a class="page-link" href="#">
-                                    <i class='bx bx-chevrons-right'></i>
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
                 </div>
             </div>
         </div>
